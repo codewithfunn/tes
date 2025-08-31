f39bfc7324a37dd8201fb38f68d8fc2b75c99a48
jaspreet_suri <60849699+codewithfunn@users.noreply.github.com>
2025-09-01 04:39:59 +0530

    add build

---
+++ build/assets/js/a8aec493.fc212530.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[355],{8386:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/tes/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
